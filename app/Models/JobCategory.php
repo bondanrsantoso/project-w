1c39c4a4a8cfd6d698ce7fdd841132a3997ae1e4
Bondan Rachmat Santoso <bondanrsantoso@gmail.com>
2022-11-19 16:35:47 +0700

    Added training event count

--- app/Models/JobCategory.php
+++ app/Models/JobCategory.php
@@ -30,13 +30,36 @@ class JobCategory extends Model
         return $this->hasMany(Job::class, 'job_category_id', 'id');
     }
 
+    public function trainingEvents()
+    {
+        return $this->hasMany(TrainingEvent::class, "category_id", "id");
+    }
+
     public function jobCount(): Attribute
     {
+        if (!($this->id ?? false)) {
+            return Attribute::make(get: fn ($value) => null);
+        }
+
         $jobsCount = $this->jobs()->count();
         return Attribute::make(function ($value) use ($jobsCount) {
             return $jobsCount;
         });
     }
 
-    protected $appends = ["job_count"];
+    public function trainingEventCount(): Attribute
+    {
+        if (!($this->id ?? false)) {
+            return Attribute::make(get: fn ($value) => null);
+        }
+
+        $eventCount = $this->trainingEvents()->count();
+        return Attribute::make(
+            get: function ($valud) use ($eventCount) {
+                return $eventCount;
+            }
+        );
+    }
+
+    protected $appends = ["job_count", "training_event_count"];
 }
