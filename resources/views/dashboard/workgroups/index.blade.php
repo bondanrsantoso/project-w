20f196e61bc4450e3f6bf615553aee13994ca1e3
Bondan Rachmat Santoso <bondanrsantoso@gmail.com>
2022-12-20 22:36:50 +0700

    Fix ORM property referencing

--- resources/views/dashboard/workgroups/index.blade.php
+++ resources/views/dashboard/workgroups/index.blade.php
@@ -1,82 +1,112 @@
 @extends('dashboard.index')
 
 @section('header')
-@if(Session::has('success'))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-    {{ session('success') }}
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-  </div>
-@endif
-<h3>Workgroups</h3>
+    @if (Session::has('success'))
+        <div
+            class="alert alert-success alert-dismissible fade show"
+            role="alert"
+        >
+            {{ session('success') }}
+            <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="alert"
+                aria-label="Close"
+            ></button>
+        </div>
+    @endif
+    <h3>Workgroups</h3>
 @endsection
 
 @section('content')
-<section class="section">
-    <div class="card">
-        <div class="card-header d-flex justify-content-between">
-            <h5>Data Workgroups</h5>
-            @php
-                
-            @endphp
-            <a class="btn btn-primary" href="{{ $route }}">
-                <span>
-                    <i class="bi bi-pencil me-2"></i>
-                    Create Workgroup
-                </span>
-            </a>
-        </div>
-        <div class="card-body">
-            <table class="table" id="table1">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Project ID</th>
-                        <th>Created At</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($workgroups as $workgroup)
+    <section class="section">
+        <div class="card">
+            <div class="card-header d-flex justify-content-between">
+                <h5>Data Workgroups</h5>
+                @php
+
+                @endphp
+                <a
+                    class="btn btn-primary"
+                    href="{{ $route }}"
+                >
+                    <span>
+                        <i class="bi bi-pencil me-2"></i>
+                        Create Workgroup
+                    </span>
+                </a>
+            </div>
+            <div class="card-body">
+                <table
+                    class="table"
+                    id="table1"
+                >
+                    <thead>
                         <tr>
-                            <td>{{ $workgroup['name'] }}</td>
-                            <td>{{ $workgroup['project']->name }}</td>
-                            <td>{{ $workgroup['created_at'] }}</td>
-                            <td class="d-flex justify-content-start align-items-center">
-                                <a href="{{ env('APP_DOMAIN_PM','http://pm-admin.docu.web.id') }}/dashboard/workgroups/{{ $workgroup['id'] }}/jobs" class="btn btn-primary  me-2">
-                                    <span>
-                                        <i class="bi bi-briefcase"></i>
-                                    </span>
-                                </a>
-                                <a href="{{ env('APP_DOMAIN_PM','http://pm-admin.docu.web.id') }}/dashboard/workgroups/{{ $workgroup['id'] }}/edit" class="btn btn-success  me-2">
-                                    <span>
-                                        <i class="bi bi-pencil-square"></i>
-                                    </span>
-                                </a>
-                                <form action="{{ env('APP_DOMAIN_PM','http://pm-admin.docu.web.id') }}/dashboard/workgroups/{{ $workgroup['id'] }}" method="POST">
-                                    @csrf
-                                    <input type="hidden" name="_method" value="DELETE" />
-                                    <button type="submit" class="btn btn-danger">
-                                        <i class="bi bi-trash3"></i>
-                                    </button>
-                                </form>
-                            </td>
+                            <th>Name</th>
+                            <th>Project ID</th>
+                            <th>Created At</th>
+                            <th>Action</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach ($workgroups as $workgroup)
+                            <tr>
+                                <td>{{ $workgroup->name }}</td>
+                                <td>{{ $workgroup->project->name }}</td>
+                                <td>{{ $workgroup->created_at }}</td>
+                                <td class="d-flex justify-content-start align-items-center">
+                                    <a
+                                        href="{{ env('APP_DOMAIN_PM', 'http://pm-admin.docu.web.id') }}/dashboard/workgroups/{{ $workgroup->id }}/jobs"
+                                        class="btn btn-primary  me-2"
+                                    >
+                                        <span>
+                                            <i class="bi bi-briefcase"></i>
+                                        </span>
+                                    </a>
+                                    <a
+                                        href="{{ env('APP_DOMAIN_PM', 'http://pm-admin.docu.web.id') }}/dashboard/workgroups/{{ $workgroup->id }}/edit"
+                                        class="btn btn-success  me-2"
+                                    >
+                                        <span>
+                                            <i class="bi bi-pencil-square"></i>
+                                        </span>
+                                    </a>
+                                    <form
+                                        action="{{ env('APP_DOMAIN_PM', 'http://pm-admin.docu.web.id') }}/dashboard/workgroups/{{ $workgroup->id }}"
+                                        method="POST"
+                                    >
+                                        @csrf
+                                        <input
+                                            type="hidden"
+                                            name="_method"
+                                            value="DELETE"
+                                        />
+                                        <button
+                                            type="submit"
+                                            class="btn btn-danger"
+                                        >
+                                            <i class="bi bi-trash3"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
 
-            {{ $workgroups->links() }}
+                {{ $workgroups->links() }}
 
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
 
 @section('scripts')
-<script>
-    $(document).ready(function() {
-        console.log("aaaa");
-        $('#body').css('background-color', '#FCF9BE');
-    });
-</script>
+    <script>
+        $(document).ready(function() {
+            console.log("aaaa");
+            $('#body').css('background-color', '#FCF9BE');
+        });
+    </script>
 @endsection
