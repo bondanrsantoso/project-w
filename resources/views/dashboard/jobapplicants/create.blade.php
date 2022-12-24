21a27a44f9e7a1a0a59feb9eee6459b48364deff
Bondan Rachmat Santoso <bondanrsantoso@gmail.com>
2022-12-24 10:25:49 +0700

    Fixes typos

--- resources/views/dashboard/jobapplicants/create.blade.php
+++ resources/views/dashboard/jobapplicants/create.blade.php
@@ -1,58 +1,73 @@
 @extends('dashboard.index')
 
 @section('header')
-<h3>Create Project</h3>
+    <h3>Create Job Application</h3>
 @endsection
 
 @section('content')
-<section id="basic-horizontal-layouts">
-    <div class="card">
-        <div class="card-content">
-            <div class="card-body">
-                <form class="form form-horizontal" action="{{ env('APP_DOMAIN_JOB','http://job-admin.docu.web.id') }}/dashboard/job-applications" method="POST">
-                    @csrf
+    <section id="basic-horizontal-layouts">
+        <div class="card">
+            <div class="card-content">
+                <div class="card-body">
+                    <form
+                        class="form form-horizontal"
+                        action="{{ env('APP_DOMAIN_JOB', 'http://job-admin.docu.web.id') }}/dashboard/job-applications"
+                        method="POST"
+                    >
+                        @csrf
 
-                    <div class="form-body">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <label>Worker ID</label>
-                            </div>
-                            <div class="col-md-8 form-group">
-                                <select class="form-select" name="worker_id" aria-label="Default select example">
-                                    @foreach ($workers as $worker)
-                                    <option value="{{ $worker['id'] }}">{{ $worker['user']->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            
-                            <div class="col-md-4">
-                                <label>Job ID</label>
-                            </div>
-                            <div class="col-md-8 form-group">
-                                <select class="form-select" name="job_id" aria-label="Default select example">
-                                    @foreach ($jobs as $job)
-                                    <option value="{{ $job['id'] }}">{{ $job['name'] }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
+                        <div class="form-body">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label>Worker ID</label>
+                                </div>
+                                <div class="col-md-8 form-group">
+                                    <select
+                                        class="form-select"
+                                        name="worker_id"
+                                        aria-label="Default select example"
+                                    >
+                                        @foreach ($workers as $worker)
+                                            <option value="{{ $worker['id'] }}">{{ $worker['user']->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
 
-                            <div class="col-sm-12 d-flex justify-content-end">
-                                <button type="submit" class="btn btn-primary btn-lg me-1 px-3 mb-1">Submit</button>
+                                <div class="col-md-4">
+                                    <label>Job ID</label>
+                                </div>
+                                <div class="col-md-8 form-group">
+                                    <select
+                                        class="form-select"
+                                        name="job_id"
+                                        aria-label="Default select example"
+                                    >
+                                        @foreach ($jobs as $job)
+                                            <option value="{{ $job['id'] }}">{{ $job['name'] }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <div class="col-sm-12 d-flex justify-content-end">
+                                    <button
+                                        type="submit"
+                                        class="btn btn-primary btn-lg me-1 px-3 mb-1"
+                                    >Submit</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
 
 @section('scripts')
-<script>
-    $(document).ready(function() {
-        console.log("aaaa");
-        $('#body').css('background-color', '#CE7777');
-    });
-</script>
+    <script>
+        $(document).ready(function() {
+            console.log("aaaa");
+            $('#body').css('background-color', '#CE7777');
+        });
+    </script>
 @endsection
--- resources/views/dashboard/jobapplicants/detail.blade.php
+++ resources/views/dashboard/jobapplicants/detail.blade.php
@@ -1,84 +1,133 @@
 @extends('dashboard.index')
 
 @section('header')
-<h3>Create Project</h3>
+    <h3>Edit Job Applications</h3>
 @endsection
 
 @section('content')
-<section id="basic-horizontal-layouts">
-    <div class="card">
-        <div class="card-content">
-            <div class="card-body">
-                <form class="form form-horizontal" action="{{ env('APP_DOMAIN_JOB','http://job-admin.docu.web.id') }}/dashboard/job-applications/{{ $jobApplication['id'] }}" method="POST">
-                    @csrf
+    <section id="basic-horizontal-layouts">
+        <div class="card">
+            <div class="card-content">
+                <div class="card-body">
+                    <form
+                        class="form form-horizontal"
+                        action="{{ env('APP_DOMAIN_JOB', 'http://job-admin.docu.web.id') }}/dashboard/job-applications/{{ $jobApplication['id'] }}"
+                        method="POST"
+                    >
+                        @csrf
 
-                    <input type="hidden" name="_method" value="PUT">
-                    <div class="form-body">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <label>Worker ID</label>
-                            </div>
-                            <div class="col-md-8 form-group">
-                                <select class="form-select" name="worker_id" aria-label="Default select example">
-                                    @foreach ($workers as $worker)
-                                    <option {{ $worker['id'] == $jobApplication['worker_id'] ? "selected" : "" }} value="{{ $worker['id'] }}">{{ $worker['user']->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            
-                            <div class="col-md-4">
-                                <label>Job ID</label>
-                            </div>
-                            <div class="col-md-8 form-group">
-                                <select class="form-select" name="job_id" aria-label="Default select example">
-                                    @foreach ($jobs as $job)
-                                    <option {{ $job['id'] == $jobApplication['worker_id'] ? "selected" : "" }} value="{{ $job['id'] }}">{{ $job['name'] }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-
-                            <div class="col-md-4">
-                                <label>Is Hired</label>
-                            </div>
-                            <div class="col-md-8 form-group">
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" {{ $jobApplication['is_hired'] == 1 ? "checked" : "" }} type="radio" name="is_hired" id="inlineRadio1" value="1">
-                                    <label class="form-check-label" for="inlineRadio1">True</label>
+                        <input
+                            type="hidden"
+                            name="_method"
+                            value="PUT"
+                        >
+                        <div class="form-body">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label>Worker ID</label>
                                 </div>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" {{ $jobApplication['is_hired'] == 0 ? "checked" : "" }} type="radio" name="is_hired" id="inlineRadio2" value="0">
-                                    <label class="form-check-label" for="inlineRadio2">False</label>
+                                <div class="col-md-8 form-group">
+                                    <select
+                                        class="form-select"
+                                        name="worker_id"
+                                        aria-label="Default select example"
+                                    >
+                                        @foreach ($workers as $worker)
+                                            <option
+                                                {{ $worker['id'] == $jobApplication['worker_id'] ? 'selected' : '' }}
+                                                value="{{ $worker['id'] }}"
+                                            >{{ $worker['user']->name }}</option>
+                                        @endforeach
+                                    </select>
                                 </div>
-                            </div>
 
-                            <div class="col-md-4">
-                                <label>Status</label>
-                            </div>
-                            <div class="col-md-8 form-group">
-                                <select class="form-select" name="status" aria-label="Default select example">
-                                    <option value="on_progress">On Progress</option>
-                                    <option value="done">Done</option>
-                                    <option value="cancelled">Cancelled</option>
-                                </select>
-                            </div>
+                                <div class="col-md-4">
+                                    <label>Job ID</label>
+                                </div>
+                                <div class="col-md-8 form-group">
+                                    <select
+                                        class="form-select"
+                                        name="job_id"
+                                        aria-label="Default select example"
+                                    >
+                                        @foreach ($jobs as $job)
+                                            <option
+                                                {{ $job['id'] == $jobApplication['worker_id'] ? 'selected' : '' }}
+                                                value="{{ $job['id'] }}"
+                                            >{{ $job['name'] }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
 
-                            <div class="col-sm-12 d-flex justify-content-end mt-2">
-                                <button type="submit" class="btn btn-primary btn-lg me-1 px-3 mb-1">Submit</button>
+                                <div class="col-md-4">
+                                    <label>Is Hired</label>
+                                </div>
+                                <div class="col-md-8 form-group">
+                                    <div class="form-check form-check-inline">
+                                        <input
+                                            class="form-check-input"
+                                            {{ $jobApplication['is_hired'] == 1 ? 'checked' : '' }}
+                                            type="radio"
+                                            name="is_hired"
+                                            id="inlineRadio1"
+                                            value="1"
+                                        >
+                                        <label
+                                            class="form-check-label"
+                                            for="inlineRadio1"
+                                        >True</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input
+                                            class="form-check-input"
+                                            {{ $jobApplication['is_hired'] == 0 ? 'checked' : '' }}
+                                            type="radio"
+                                            name="is_hired"
+                                            id="inlineRadio2"
+                                            value="0"
+                                        >
+                                        <label
+                                            class="form-check-label"
+                                            for="inlineRadio2"
+                                        >False</label>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <label>Status</label>
+                                </div>
+                                <div class="col-md-8 form-group">
+                                    <select
+                                        class="form-select"
+                                        name="status"
+                                        aria-label="Default select example"
+                                    >
+                                        <option value="on_progress">On Progress</option>
+                                        <option value="done">Done</option>
+                                        <option value="cancelled">Cancelled</option>
+                                    </select>
+                                </div>
+
+                                <div class="col-sm-12 d-flex justify-content-end mt-2">
+                                    <button
+                                        type="submit"
+                                        class="btn btn-primary btn-lg me-1 px-3 mb-1"
+                                    >Submit</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
 
 @section('scripts')
-<script>
-    $(document).ready(function() {
-        console.log("aaaa");
-        $('#body').css('background-color', '#CE7777');
-    });
-</script>
+    <script>
+        $(document).ready(function() {
+            console.log("aaaa");
+            $('#body').css('background-color', '#CE7777');
+        });
+    </script>
 @endsection
