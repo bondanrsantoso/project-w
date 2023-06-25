9b7a7d49d87ee1e14e7cdd00ec91b60ee212a632
Bondan Rachmat Santoso <bondanrsantoso@gmail.com>
2023-06-25 21:06:15 +0700

    Fix worker attribute

--- app/Models/Worker.php
+++ app/Models/Worker.php
@@ -74,13 +74,14 @@ class Worker extends Model
 
     public function isEligibleForWork(): Attribute
     {
-        if (!($this->id ?? false)) {
-            return Attribute::make(get: fn ($value) => null);
-        }
+        // if (!($this->id ?? false)) {
+        //     return Attribute::make(get: fn ($value) => null);
+        // }
 
-        $achievements = $this->achievements()->first();
+        // $achievements = $this->achievements()->first();
 
-        return Attribute::make(get: function ($value) use ($achievements) {
+        return Attribute::make(get: function ($value, $attributes) {
+            $achievements = Achievement::where("worker_id", $attributes["id"])->first();
             return $achievements && $value;
         })->shouldCache();
     }
