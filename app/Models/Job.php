3202e76496acdc7db7cc44d783493d5c3002f295
Bondan Rachmat Santoso <bondanrsantoso@gmail.com>
2023-06-25 20:39:32 +0700

    Fix company attribute

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -115,12 +115,15 @@ class Job extends Model
     {
         return Attribute::make(
             get: function ($value, $attributes) {
-                // $company =
-                //     Company::whereRelation("projects.workgroups.jobs", "id", $attributes["id"])
-                //     ->first();
+                Log::info(json_encode($attributes));
+                Log::info(Company::whereRelation("projects.workgroups.jobs", "id", $attributes["id"])->toSql());
+                $company =
+                    Company::whereRelation("projects.workgroups.jobs", "id", $attributes["id"])
+                    ->first();
+                Log::info(json_encode($company));
 
 
-                return null;
+                return $company;
             }
         );
 
